b49a7ff46d8bb17ca8df62567f7ac90305b05960
Виталий <@172.19.248.52>
2021-07-30 20:09:57 +0300

    renamed WevChatApplication

--- web-chat/src/main/java/ua/step/pokrivchak/webchat/WebChatApplication.java
+++ web-chat/src/main/java/ua/step/pokrivchak/webchat/WebChatApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WevChatApplication {
+public class WebChatApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(WevChatApplication.class, args);
+		SpringApplication.run(WebChatApplication.class, args);
 	}
 }
